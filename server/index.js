2de8e1dafbe83751a7dd5d6188f31ff7ef92d576
Aditya Soni <adityanithariya@gmail.com>
2023-06-19 23:09:19 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
